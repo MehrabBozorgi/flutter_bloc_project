54e6a77e201eb4397967e0a725530548ff26d99f
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-12-28 12:47:42 +0330

    bloc_api

---
+++ lib/user/services/user_repository.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_call_api_bloc/user/services/user_api_service.dart';
+
+import '../model/user_model.dart';
+
+class UserRepository {
+
+  UserApiService userApiService = UserApiService();
+
+  Future<List<UserModel>> fetchUsers() async {
+    Response response = await userApiService.callUserApi();
+
+    List<UserModel> loaded =[];
+
+    if (response.statusCode == 200) {
+      List data = response.data;
+      for (int i = 0; i < data.length; i++) {
+        final result = UserModel.fromJson(data[i]);
+        loaded.add(result);
+      }
+    }
+
+    return loaded;
+  }
+}
