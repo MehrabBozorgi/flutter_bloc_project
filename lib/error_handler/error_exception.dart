af4289945cf54c429ece44ecebd157d72d00007c
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-12-31 12:13:53 +0330

    bloc_api

---
+++ lib/error_handler/error_exception.dart
@@ -0,0 +1,72 @@
+
+import 'package:dio/dio.dart';
+
+class ErrorExceptions implements Exception {
+  fromError(DioError dioError) {
+
+    switch (dioError.type) {
+      case DioErrorType.connectTimeout:
+        return "Connection timeout with API server";
+      case DioErrorType.sendTimeout:
+        return "Connection timeout with API server";
+      case DioErrorType.receiveTimeout:
+        return "Receive timeout in connection with API server";
+      case DioErrorType.response:
+        return handleError(dioError.response?.statusCode);
+      case DioErrorType.cancel:
+        return "Request to API server was cancelled";
+      case DioErrorType.other:
+        return "Connection to API server failed due to internet connection";
+    }
+  }
+
+  String handleError(int? statusCode) {
+    switch (statusCode) {
+      case 400:
+        return 'Bad request';
+      case 404:
+        return 'not found';
+      case 500:
+        return 'Internal server error';
+      default:
+        return 'Oops something went wrong';
+    }
+  }
+}
+
+//   fromDioError(DioError dioError) {
+//     switch (dioError.type) {
+//       case DioErrorType.cancel:
+//         return "Request to API server was cancelled";
+//       case DioErrorType.connectTimeout:
+//         return "Connection timeout with API server";
+//       case DioErrorType.other:
+//         return "Connection to API server failed due to internet connection";
+//       case DioErrorType.receiveTimeout:
+//         return "Receive timeout in connection with API server";
+//       // case DioErrorType.response:
+//       //   return handleError(dioError.response?.statusCode);
+//       case DioErrorType.sendTimeout:
+//         return "Send timeout in connection with API server";
+//       default:
+//         return "Something went wrong";
+//     }
+//   }
+//
+//   late String message;
+//
+String handleError(int? statusCode) {
+  switch (statusCode) {
+    case 400:
+      return 'Bad request';
+    case 404:
+      return '404';
+    case 500:
+      return 'Internal server error';
+    default:
+      return 'Oops something went wrong';
+  }
+}
+//
+//   @override
+//   String toString() => message;
